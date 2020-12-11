da8917282bad6ccff6d7ac6d7e4d3f7dc0a4ba97
Roberth <rrondoc@gmail.com>
2020-12-10 22:15:35 -0500

    Uploading task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    lenght = len(argv)
+    if lenght == 1:
+        print("0 arguments:")
+    elif lenght > 2:
+        print("{:d} arguments:".format(lenght - 1))
+    else:
+        print("1 argument:")
+    for i in range(1, lenght):
+        print("{:d}: {:s}".format(i, argv[i]))
