a8de1ca8b05b88d2cfc610e5d4666a68bf57ff0f
Roberth <rrondoc@gmail.com>
2020-12-15 14:33:14 -0500

    Updating task 2 style pep8

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 def replace_in_list(my_list, idx, element):
     lenght = len(my_list)
     if idx < 0 or idx > (lenght - 1):
-        return my_lyst
-            my_list[idx] = element
-            return(my_list)
+        return my_list
+    my_list[idx] = element
+    return(my_list)
