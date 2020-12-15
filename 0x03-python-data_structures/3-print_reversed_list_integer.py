4b31711ff56c546aa4e63f08885aa4b5eae8aef5
Roberth <rrondoc@gmail.com>
2020-12-15 13:40:39 -0500

    Uploading Proyect 0x03 Phyton

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    lenght = len(my_list)
+    my_list.reverse()
+    for i in range (lenght):
+        print("{:d}".format(my_list[i]))
+
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    lenght = len(my_list)
+
+    my_list1 = my_list.copy()
+
+    my_list1[idx] = element
+
+    if idx < 0:
+        return my_list1
+    elif idx > lenght:
+        return my_list1
+    return(my_list1)
+
