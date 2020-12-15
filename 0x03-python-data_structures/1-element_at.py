0694fd76198cf7f9ae0df977573a96ca67a8d3e8
Roberth <rrondoc@gmail.com>
2020-12-15 13:50:13 -0500

    Updating task 0 style pep8

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     lenght = len(my_list)
     if idx < 0:
-        return
+        return None
     elif idx > lenght:
-        return
+        return None
     else:
         return(my_list[idx])
