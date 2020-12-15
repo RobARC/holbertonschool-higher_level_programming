a6ab4b6360acc88dd22821530aa295f91d164b1f
Roberth <rrondoc@gmail.com>
2020-12-15 14:25:44 -0500

    Updating task 1 style pep8

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     lenght = len(my_list)
-    if idx < 0 or idx > lenght:
+    if idx < 0 or idx > (lenght - 1):
         return my_lyst
     for i in range(lenght):
         if i == idx:
