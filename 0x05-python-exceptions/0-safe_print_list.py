c28cff43c89cab9c13059d089ee322b9cd2bff13
Roberth <rrondoc@gmail.com>
2021-01-04 14:57:55 -0500

    Updating task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     if my_list is None or my_list == [] or x <= 0:
         print()
@@ -8,8 +9,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
         print()
-    except:
+    except ValueError:
         print()
         return (i)
     return (i + 1)
-
