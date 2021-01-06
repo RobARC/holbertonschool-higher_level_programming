8887e1e88aabcab9409461a776faf7f666701024
Roberth <rrondoc@gmail.com>
2021-01-05 22:54:04 -0500

    Uploading task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    @property
+    def size(self):
+        """Size getter, return the private size"""
+
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Size setter, sets the size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter for position of square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for position square"""
+        if (type(position) != tuple or len(position) != 2 or
+                type(position[0]) != int or type(position[1]) != int or
+                position[0] < 0 or position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """print square to stdout with character #"""
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            c = "#"
+            for j in range(self.__size):
+                print(" " * self.__position[0], end='')
+                print(c * self.__size)
