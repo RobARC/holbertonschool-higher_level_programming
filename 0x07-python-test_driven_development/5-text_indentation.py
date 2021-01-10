c89c14ed834283cef2a0a9766fa58e62744f9b24
Roberth <rrondoc@gmail.com>
2021-01-10 12:49:53 -0500

    Updating task 4 file.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,13 @@
 #!/bin/usr/python3
 """module text_identation"""
 
- 
-    def text_indentation(text):
+
+def text_indentation(text):
     """function that prints a text with 2 new lines
     after each of these characters: ., ? and :
     """
 
     b = 0
-<<<<<<< HEAD
     if type(text) != str:
         raise TypeError("text must be a string")
 
@@ -20,21 +19,4 @@
             if (b + 1) < lenght:
                 while text[b + 1] is " ":
                     b += 1
-=======
-    c = '?'
-    d = ':'
-  
-    if type(text) != str:
-        raise TypeError("text must be a string")
-        
-        lenght = len(text)
-        while b < lenght:
-            print(text[b], end="")
-            if text[b] is a or text[b] is c or text[b] is d:
-                print("\n")
-                if (b + 1) < lenght:
-                    while text[b + 1] is " ":
-                        b += 1
->>>>>>> 744a3fd5d9c8503f94fa4b5e3defe447397bc660
         b += 1
-
