0dbeaf7cd3e03ef6e3de8c553ad4ab27d6a4e5ca
Roberth <rrondoc@gmail.com>
2021-01-10 16:14:53 -0500

    Uploading task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestFunctionMaxInteger(unittest.TestCase):
+    """
+    Define a Class to test all diferent cases in
+    6-max_integer.py
+    """
+    def test_integer_num(self):
+        """ Checks for integer numbers"""
+        # Positive Numbers
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+        # Negative Numbers
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+        # Negative and Positive
+        self.assertEqual(max_integer([1, -2, -3, 4]), 4)
+
+        # One integer
+        self.assertEqual(max_integer([3]), 3)
+
+        # One negative integer
+        self.assertEqual(max_integer([-3]), -3)
+
+    def test_float_num(self):
+        # Positive Float Numbers
+        self.assertEqual(max_integer([1.8, 2.24, 3.35, 4.4]), 4.4)
+        # Negative Float Numbers
+        self.assertEqual(max_integer([-1.8, -2.24, -3.35, -4.4]),     -1.8)
+
+    def test_strings(self):
+        """Checks for strings like arguments"""
+        self.assertEqual(max_integer(['Roberth']), 'Roberth')
