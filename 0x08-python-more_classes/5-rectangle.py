c2658f11e10a86d322b959be85d4181affe69ccf
Roberth <rrondoc@gmail.com>
2021-01-11 21:46:40 -0500

    updating task 5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,20 +1,51 @@
 #!/usr/bin/python3
-"""Rectangle Module"""
+"""This module create a Rectangle"""
 
 
 class Rectangle:
-    """Rectangle class"""
+    """This class define a Rectangle object"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        """Method init initialize objects"""
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """getter method, return private height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height.setter, sets height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """getter method, return private width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """with setter, sets width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     def area(self):
-        area = self.__width * self.__height
+        """Return rectangle area"""
+        area = self.width * self.height
         return area
 
     def perimeter(self):
-        perimeter = 2 * (self.__width + self.__height)
-        if self.__width == 0 or self.__height == 0:
+        """Return rectangle perimeter"""
+        perimeter = 2 * (self.width + self.height)
+        if self.width == 0 or self.height == 0:
             perimeter = 0
         return perimeter
 
@@ -31,33 +62,3 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-
-    @property
-    def height(self):
-        """getter method, return private height"""
-
-        return (self.height)
-
-    @height.setter
-    def height(self, value):
-        """height.setter, sets height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @property
-    def width(self):
-        """getter method, return private width"""
-
-        return (self.width)
-
-    @width.setter
-    def width(self, value):
-        """with setter, sets width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
