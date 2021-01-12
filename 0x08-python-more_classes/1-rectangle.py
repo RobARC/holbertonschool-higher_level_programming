ed503f4c626806628c26d8f0ab444495ada2d4f5
Roberth <rrondoc@gmail.com>
2021-01-11 21:05:50 -0500

    updating task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """This class define a Rectangle object"""
     def __init__(self, width=0, height=0):
         """Method init inicialize objects"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
