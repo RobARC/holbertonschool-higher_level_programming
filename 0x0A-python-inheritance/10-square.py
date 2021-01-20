ac1cc2aa6512be702776729bd5db34818e65ec21
Roberth <rrondoc@gmail.com>
2021-01-20 08:16:28 -0500

    uploading task 10 Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+
+
+def __init__(self, size):
+    """Method init initialize objects"""
+    self.integer_validator("size", size)
+    self.__size = size
+    super(Square, self).__init__(size, size)
+
+
+def area(self):
+    """method return square area"""
+    area = super().area
+    return area
