d93e7ecfca77752f56d32ecd1d089ef51a5efca1
Roberth <rrondoc@gmail.com>
2021-01-19 19:34:57 -0500

    Updating task0 pep8 style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+""" modulo lookup"""
+
+
 def lookup(obj):
+    """function that returns the list of
+    available attributes and methods of an object
+    """
+
     return(dir(obj))
