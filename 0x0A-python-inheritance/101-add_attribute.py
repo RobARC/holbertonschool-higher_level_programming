3ee4e15a9db30f6c5ae0029b7062fce0861745cc
Roberth <rrondoc@gmail.com>
2021-01-20 11:39:01 -0500

    uploading task 13 Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(obj, attribute, value):
     """function Add an attribute to an object if it's possible"""
 
-    if obj is None or attribute is None or value is None:
+    if '__add__' in dir(obj):
         raise TypeError("can't add new attribute")
     else:
         setattr(obj, attribute, value)
