073d0aac66de8a1b5a04014a30730b07a8873e8c
Roberth <rrondoc@gmail.com>
2021-01-21 16:06:50 -0500

    uploading task 10 student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module class Student"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Method init initialize objects"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = self.__dict__[i]
+            except Exception as e:
+                pass
+        return new_dict
