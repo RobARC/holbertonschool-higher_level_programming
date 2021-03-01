eb00545f4ab55897b234b79f9d57f1264e05c5df
RobARC <rrondonc@gmail.com>
2021-03-01 22:33:37 +0000

    updating task 8

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -4,4 +4,4 @@
 -- id = 89
 -- name = Holberton School
 
-INSERT INTO `first_table` ((`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
