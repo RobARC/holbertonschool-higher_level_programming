a754145e9393367df58409e18c41f128f62078f5
RobARC <rrondonc@gmail.com>
2021-03-02 23:05:03 +0000

    uploading task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,4 +7,4 @@
 -- If the table cities already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT FOREIGN KEY(state_id) REFERENCES states(id));
+CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT, FOREIGN KEY(state_id) REFERENCES states(id));
