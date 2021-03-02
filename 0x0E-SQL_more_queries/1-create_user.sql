ca5e2b1aeda9b6a759fa0fc8524e19c928f23758
RobARC <rrondonc@gmail.com>
2021-03-02 20:39:22 +0000

    updating task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- script that creates the MySQL server user user_0d_1.
+-- If the database hbtn_0c_0 already exists, your script should not fail
 
-CREATE IF EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE IF NOT EXISTS USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
