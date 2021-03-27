45262d0e7e9cff3a8d54350c4760abd05972fd92
root <root@DESKTOP-EIB5D01.localdomain>
2021-03-27 13:55:14 -0500

    uploading task 11 insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" script that lists all State objects from the database hbtn_0e_6_usa """
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(mysql_username,
+          mysql_password, database_name))
+
+    engine = create_engine(db, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name='Lousiana')
+
+    session.add(state)
+    session.commit()
+
+    print(state.id)
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
