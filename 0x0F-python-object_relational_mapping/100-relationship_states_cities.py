f47ac350daa5e558b86c4f10297c9a4ce73ae895
root <root@DESKTOP-EIB5D01.localdomain>
2021-04-01 12:28:54 -0500

    update task 15 uploading files relationship and update file 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,45 +1,47 @@
 #!/usr/bin/python3
-""" script that creates the State “California” with the City “San Francisco” 
+""" script that creates the State “California” with the City “San Francisco”
     from the database hbtn_0e_100_usa
 """
 
 import sys
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import relationship, sessionmaker
 
 
-def create_state_and_city():
+def main():
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
-    mysql_username = sys.argv[3]
+    database_name = sys.argv[3]
 
     # conector to data base with parameters recivied from console
-    db = ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(mysql_username, 
+    db = ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(mysql_username,
           mysql_password, database_name))
 
-    # engine conection 
+    # engine conection
     engine = create_engine(db, pool_pre_ping=True)
 
     # Creates all classes currently active.
-    Base.metada.create_all(engine)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    
-    #create session's cursor.
+
+    # create session's cursor.
     session = Session()
 
     newState = State(name="California", cities=[City(name="San Francisco")])
 
-    #Add the new state.
+    # Add the new state.
     session.add(newState)
 
-    #Commit the change.
+    # Commit the change.
     session.commit()
 
-    #Close session.
+    # Close session.
 
     session.close()
 
 
+if __name__ == "__main__":
+    main()
