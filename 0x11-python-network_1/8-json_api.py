224d91c69ebf9a34a298e30f07ce75c50035f9f8
RobARC <rrondonc@gmail.com>
2021-04-11 17:15:17 +0000

    uploading task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""  script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+
+def main(argv):
+
+    url = 'http://f1859af609d9.b380b380.hbtn-cod.io:5000/search_user'
+
+    if len(argv) < 2:
+        q = ""
+    else:
+        q = argv[1]
+
+    payload = {'q': q}
+    req = requests.post(url, data=payload)
+    try:
+        response = req.json()
+        if bool(response):
+            print("[{}] {}".format(response['id'], response['name']))
+        else:
+            print("No result")
+    except ValueError:
+            print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main(argv)
