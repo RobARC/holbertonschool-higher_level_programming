a469159ae0721d2d0238f02382074c230caed1d0
Roberth <rrondoc@gmail.com>
2021-05-06 16:18:20 -0500

    updating semistandar style file 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,11 +4,10 @@ const argvs = process.argv.slice(2);
 const filename = argvs[0];
 const fs = require('fs');
 
-
 fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;  
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
