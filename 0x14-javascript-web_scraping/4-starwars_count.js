abc429b03cb0688f53999f135dc18099e5867587
Roberth <rrondoc@gmail.com>
2021-05-07 10:23:57 -0500

    uploading task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const GET = request.get;
+const url = process.argv[2];
+
+GET(url, (err, response, body) => {
+  if (err) return (console.log(err));
+
+  const movies = JSON.parse(body).results;
+  let count = 0;
+
+  for (let i = 0; i < movies.length; i++) {
+    for (let j = 0; j < movies[i].characters.length; j++) {
+      if (movies[i].characters[j].includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
