15f3581249fabbbe24fda4d327d62db058f2f684
RobARC <rrondonc@gmail.com>
2021-06-15 17:02:58 +0000

    updating task 20 Best Genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -8,4 +8,4 @@ SELECT tv_genres.name, sum(tv_show_ratings.rate) AS rating
 FROM tv_show_genres 
 RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
 LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id 
-GROUP BY tv_genres.name ORDER BY rating DESC;
+GROUP BY tv_genres.name ORDER BY rating DESC tv_genres.name;
