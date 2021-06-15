3def2f905a2a6b4e0676fad99a07440b5c57bca1
RobARC <rrondonc@gmail.com>
2021-06-15 14:57:34 +0000

    updating task 18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,4 +5,14 @@
 -- You can use a maximum of two SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT DISTINCT tvs.title FROM tv_shows AS tvs WHERE tvs.title NOT IN (SELECT tvs.title as title FROM tv_genres AS tvg INNER JOIN tv_show_genres AS tvsg ON tvsg.genre_id = tvg.id INNER JOIN tv_shows AS tvs ON tvsg.show_id = tvs.id WHERE tvg.name = "Comedy") ORDER BY title ASC; 
+SELECT DISTINCT tvs.title
+FROM tv_shows AS tvs
+WHERE tvs.title NOT IN 
+(SELECT tvs.title AS title 
+	FROM tv_genres AS tvg 
+	INNER JOIN tv_show_genres AS tvsg 
+	ON tvsg.genre_id = tvg.id 
+	INNER JOIN tv_shows AS tvs 
+	ON tvsg.show_id = tvs.id 
+	WHERE tvg.name = "Comedy")
+ORDER BY title ASC; 
