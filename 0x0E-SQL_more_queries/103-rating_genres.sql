a344d0d23b6648875c9482c8814bbb536e5ddb1e
RobARC <rrondonc@gmail.com>
2021-06-15 17:08:59 +0000

    updating task 20 Best Genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,8 +4,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name, sum(tv_show_ratings.rate) AS rating 
-FROM tv_show_genres 
-RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
-LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id 
-GROUP BY tv_genres.name ORDER BY rating DESC tv_genres.name;
+SELECT tv_genres.name, sum(tv_show_ratings.rate) AS rating
+FROM tv_genres INNER JOIN tv_show_genres 
+ON  tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_show_ratings 
+ON tv_show_genres.show_id = tv_show_ratings.show_id  
+GROUP BY tv_genres.name ORDER BY rating DESC, tv_genres.name;
